8b096256bf5f4e91dd9dce6f9d86176340e33f0c
naveed <naveed.ahmed.shakir@gmail.com>
2024-09-10 07:13:11 +0530

    updated guess parse err line

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
         //let guess: u32 = guess.trim().parse().expect("Please type a number!");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => continue
         };
 
         println!("You guessed: {guess}");
